fed8b026c5aac27d03344dac463d3af3d186d28a
Phillips Gabriel <832849@stma.k12.mn.us>
2021-03-26 09:33:16 -0500

    try2

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def topFiveMovies(a, b, c, d, e):
     return a + b + c + d + e
-print(topFiveMovies("ponyo", "Kiki's delivery service", "cars"))
+print(topFiveMovies("ponyo, ", "Kiki's delivery service, ", "cars, ", "my neighbor Totero, ", "Howls moving castle"))
